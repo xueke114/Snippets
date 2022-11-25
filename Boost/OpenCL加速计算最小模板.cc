691cabb2449cf8cdeb812d9ded9baa1bd7982f20
Zheng Xueke <lengfeng1453@hotmail.com>
2022-11-25 15:46:22 +0800

    OpenCL加速计算最小模板

---
+++ Boost/OpenCL加速计算最小模板.cc
@@ -0,0 +1,40 @@
+#include<vector>
+#include<algorithm>
+#include<iostream>
+#include<boost/compute/algorithm/transform.hpp>
+#include<boost/compute/container/vector.hpp>
+#include<boost/compute/functional/math.hpp>
+
+namespace compute=boost::compute;
+
+int main() {
+
+    // 获取默认opencl设备并设置上下文
+    compute::device device=compute::system::default_device();
+    std::cout<<device.name()<<std::endl;
+    std::cout<<device.platform().name()<<std::endl;
+
+    compute::context context(device);
+    compute::command_queue queue(context,device);
+
+    // 生成随机数据
+    std::vector<float> host_vector(200000000);
+    std::generate(host_vector.begin(),host_vector.end(),rand);
+    std::cout<<host_vector[0]<<" "<<host_vector[1]<<" "<<host_vector[2]<<std::endl;
+
+    //在opencl设备上创建数据
+    compute::vector<float> device_vector(host_vector.size(),context);
+
+    // 将主机数据传输到opencl设备
+    compute::copy(host_vector.begin(),host_vector.end(),device_vector.begin(),queue);
+
+    // 在opencl设备上进行计算
+    compute::transform(device_vector.begin(),device_vector.end(),device_vector.begin(),compute::sqrt<float>(),queue);
+    // 将计算结果传输到主机
+    compute::copy(device_vector.begin(),device_vector.end(),host_vector.begin(),queue);
+
+    // 输出计算结果
+    std::cout<<host_vector[0]<<" "<<host_vector[1]<<" "<<host_vector[2]<<std::endl;
+
+    return 0;
+}
