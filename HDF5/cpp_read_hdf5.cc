d2e1bbc5ec1e7efbb1f90a97ccc272393aed3ab2
Zheng Xueke <lengfeng1453@hotmail.com>
2022-12-06 11:09:02 +0800

    更改HDF5 read的数据内存分配方式

--- HDF5/cpp_read_hdf5.cc
+++ HDF5/cpp_read_hdf5.cc
@@ -1,31 +1,30 @@
 // MSVC需要设置预处理器H5_BUILT_AS_DYNAMIC_LIB;
+#include<vector>
 #include <iostream>
 #include<H5Cpp.h>
 
-int main()
-{
-	//定义变量
-	std::string fileName = "Assets/FY3D_MWRIA_GBAL_L1_20220301_0242_010KM_MS.HDF";
-	// 打开文件
-	auto h5_obj = H5::H5File(fileName, H5F_ACC_RDONLY);
-	// 打开数据集
-	auto lat_ds = h5_obj.openDataSet("Geolocation/Latitude");
-	// 获取数据集形状
-	auto ds_space = lat_ds.getSpace();
-	auto size = ds_space.getSimpleExtentNdims();
-	auto shape = new hsize_t[size];
-	ds_space.getSimpleExtentDims(shape);
-	std::cout << shape[0] << " " << shape[1] << std::endl;
-	// 为ds分配空间
-	auto out_data = new float[1827][266];
-	// 读取ds
-	lat_ds.read(out_data, H5::PredType::NATIVE_FLOAT);
-	std::cout << out_data[0][1] << std::endl;
+int main() {
+    //定义变量
+    std::string fileName = "Assets/FY3D_MWRIA_GBAL_L1_20220301_0242_010KM_MS.HDF";
+    // 打开文件
+    auto h5_obj = H5::H5File(fileName, H5F_ACC_RDONLY);
+    // 打开数据集
+    auto lat_ds = h5_obj.openDataSet("Geolocation/Latitude");
+    // 获取数据集形状
+    auto ds_space = lat_ds.getSpace();
+    auto size = ds_space.getSimpleExtentNdims();
+    auto shape = new hsize_t[size];
+    ds_space.getSimpleExtentDims(shape);
+    std::cout << "Height: " << shape[0] << " Width: " << shape[1] << std::endl;
+    // 为ds分配空间
+    std::vector<float> out_data(shape[0]*shape[1]);
+    // 读取ds
+    lat_ds.read(out_data.data(), H5::PredType::NATIVE_FLOAT);
+    std::cout << out_data[1] << std::endl;
 
-	//关闭句柄
-	delete[] shape;
-	delete[] out_data;
-	ds_space.close();
-	lat_ds.close();
-	h5_obj.close();
+    //关闭句柄
+    delete[] shape;
+    ds_space.close();
+    lat_ds.close();
+    h5_obj.close();
 }
