cdd5c8a6704a5fb75487063bd678a030219456b0
Zheng Xueke <xueke0114@foxmail.com>
2023-09-25 19:43:00 +0800

    HDF5: add CmakeLists

--- HDF5/fortran_read_hdf5.f90
+++ HDF5/fortran_read_hdf5.f90
@@ -1,11 +1,11 @@
-    ! VS + Intel Fortran ¶ÁHDF5ÎÄ¼ş
+    ! VS + Intel Fortran è¯»HDF5æ–‡ä»¶
 
     PROGRAM HDF5Demo
     USE HDF5
     IMPLICIT NONE
 
     !!!!!!!!!!!
-    ! ¶¨Òå±äÁ¿ !
+    ! å®šä¹‰å˜é‡ !
     !!!!!!!!!!!
     character(LEN=*), parameter    :: in_filename = "Assets/FY3D_MWRIA_GBAL_L1_20220301_0242_010KM_MS.HDF"
     character(LEN=*), parameter    :: ds_name = "Geolocation/Latitude"
@@ -15,26 +15,26 @@
     REAL            , ALLOCATABLE  :: lat_data(:,:)
     INTEGER                        :: error_flag
 
-    ! ¶ÁÎÄ¼ş
-    !! ½¨Á¢¾ä±ú
+    ! è¯»æ–‡ä»¶
+    !! å»ºç«‹å¥æŸ„
     call h5open_f(error_flag)
     call h5fopen_f(in_filename, H5F_ACC_RDONLY_F, file_id, error_flag)
-    !! ´ò¿ªds
+    !! æ‰“å¼€ds
     call h5dopen_f(file_id, ds_name, ds_id, error_flag)
-    !! »ñÈ¡dsĞÎ×´
+    !! è·å–dså½¢çŠ¶
     CALL h5dget_space_f(ds_id, space_id, error_flag)
     CALL h5sget_simple_extent_dims_f(space_id, real_dims, max_dims, error_flag)
     CALL h5sclose_f(space_id, error_flag)
-    !! Îªds·ÖÅä¿Õ¼ä
+    !! ä¸ºdsåˆ†é…ç©ºé—´
     ALLOCATE(lat_data(real_dims(1), real_dims(2)))
-    !! ¶ÁÈ¡ds
+    !! è¯»å–ds
     geo_dims = [real_dims(1),real_dims(2)]
     CALL h5dread_f(ds_id, H5T_NATIVE_REAL, lat_data, geo_dims, error_flag)
-    !! Êä³öµÚÒ»ÁĞµÄÇ°5ĞĞ
+    !! è¾“å‡ºç¬¬ä¸€åˆ—çš„å‰5è¡Œ
     print*,lat_data(1,1:5)
 
 
-    !! ¹Ø±Õ¾ä±ú
+    !! å…³é—­å¥æŸ„
     DEALLOCATE(lat_data)
     call h5dclose_f(ds_id, error_flag)
     call h5fclose_f(file_id, error_flag)
