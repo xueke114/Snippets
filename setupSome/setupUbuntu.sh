df74bfd0926c670ffbf1bc41c68747548e52857f
Zheng Xueke <xueke0114@foxmail.com>
2023-11-05 11:33:11 +0800

    setupSome: setupUbuntu做一些调整

--- setupSome/setupUbuntu.sh
+++ setupSome/setupUbuntu.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
+
 # 换源
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates
 echo "
-deb https://mirrors.bfsu.edu.cn/debian/ bullseye main contrib non-free
-deb-src https://mirrors.bfsu.edu.cn/debian/ bullseye main contrib non-free
-
-deb https://mirrors.bfsu.edu.cn/debian/ bullseye-updates main contrib non-free
-deb-src https://mirrors.bfsu.edu.cn/debian/ bullseye-updates main contrib non-free
-
-deb https://mirrors.bfsu.edu.cn/debian/ bullseye-backports main contrib non-free
-deb-src https://mirrors.bfsu.edu.cn/debian/ bullseye-backports main contrib non-free
-
-deb https://mirrors.bfsu.edu.cn/debian-security bullseye-security main contrib non-free
-deb-src https://mirrors.bfsu.edu.cn/debian-security bullseye-security main contrib non-free
+deb https://mirrors.cernet.edu.cn/ubuntu/ jammy main restricted universe multiverse
+deb https://mirrors.cernet.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
+deb https://mirrors.cernet.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
+deb https://mirrors.cernet.edu.cn/ubuntu/ jammy-security main restricted universe multiverse
 " | sudo tee /etc/apt/sources.list
+
 # 开启 i386 支持
 sudo dpkg --add-architecture i386
 sudo apt-get update
+
 # 安装常用软件
 ## C++ 开发
-sudo apt install -y clang-format g++ gfortran cmake ninja-build qtcreator  qtbase5-doc-html qtbase5-examples libgdal-dev qtbase5-dev
+sudo apt install -y clang-format g++ gfortran cmake ninja-build qtcreator qtbase5-doc-html qtbase5-examples libgdal-dev qtbase5-dev
 sudo apt -t bullseye-backports install libqgis-dev
 ## python 开发
-sudo apt install -y python3-openpyxl spyder  jupyter-notebook python3-pandas python3-bs4 python3-lxml python3-venv python3-cdo python3-gdal
+sudo apt install -y python3-openpyxl spyder jupyter-notebook python3-pandas python3-bs4 python3-lxml python3-venv python3-cdo python3-gdal
 ## java 开发
 sudo apt install -y default-jdk
 ## R 语言开发
@@ -31,11 +25,16 @@ sudo apt install -y r-cran-lme4 r-cran-irkernel r-cran-sp r-cran-stars r-cran-ti
 ## LaTeX 工具
 sudo apt install -y texstudio texlive-lang-chinese texlive-science texlive-pictures texlive-xetex latexmk biber 
 ## 系统工具
-sudo apt install -y ocl-icd-opencl-dev opencl-headers intel-opencl-icd clinfo vlc ffmpeg fonts-wqy-microhei baobab xfce4-weather-plugin proxychains4 command-not-found apt-file  stellarium gimp inkscape blender simplescreenrecorder peek perl wine32  winetricks  shadowsocks-libev chrony gufw bash-completion screen openssh-server fish htop
+sudo apt install -y flatpak virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso ocl-icd-opencl-dev opencl-headers intel-opencl-icd clinfo vlc ffmpeg fonts-wqy-microhei baobab proxychains4 apt-file  stellarium gimp inkscape blender simplescreenrecorder peek perl winetricks  shadowsocks-libev chrony gufw bash-completion screen openssh-server fish htop
 ## 科研工具
-sudo apt install -y cdo gdal-bin gmt libreoffice-texmaths
-sudo apt install -y -t bullseye-backports qgis libreoffice  
+sudo apt install -y cdo gdal-bin gmt qgis
+
 # 工具设置
+## 设置flatpak
+sudo apt install gnome-software-plugin-flatpak
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-modify flathub --url=https://mirror.sjtu.edu.cn/flathub
+
 ## 设置 ss-local 
 sudo apt install -y shadowsocks-libev
 sudo nano /etc/shadowsocks-libev/config.json
@@ -47,30 +46,28 @@ sudo systemctl status shadowsocks-libev.service
 sudo sed '/^socks4/d' -i /etc/proxychains4.conf
 echo "socks5 127.0.0.1 7070" | sudo tee -a /etc/proxychains4.conf
 ## 设置 wine32
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+sudo apt install --install-recommends winehq-stable
 WINEARCH=win32 WINEPREFIX=~/.wine wine wineboot
-proxychains winetricks riched20
-sudo apt-file update
-sudo update-command-not-found
 ## 设置 pip
-pip config set global.index-url https://mirrors.bfsu.edu.cn/pypi/web/simple
-## 设置 Vritualbox
-sudo apt-get install fasttrack-archive-keyring
-echo "
-deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-fasttrack main contrib
-deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-backports-staging main contrib
-" | sudo tee -a  /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso -y
+pip config set global.index-url https://mirrors.cernet.edu.cn/pypi/web/simple
 ## jupyter notebook 扩展
 proxychains jupyter nbextension install https://github.com/drillan/jupyter-black/archive/master.zip --user
 jupyter nbextension enable jupyter-black-master/jupyter-black
 ## 设置 git
 git config --global init.defaultBranch main
 git config --global user.name "Zheng Xueke"
-git config --global user.email lengfeng1453@hotmail.com
-# 需要手动安装的软件
-## Joplin
-## GitHub Desktop
-## Zotero
-## 坚果云
-# 移除多余软件
+git config --global user.email xueke0114@foxmail.com
+
+# 非apt仓库的软件
+## flatpak
+flatpak install flathub io.github.shiftey.Desktop org.hdfgroup.HDFView -y
+## snap
+sudo snap install typora zotero-snap
+## Wine版微信
+sudo apt install libjpeg62
+proxychains winetricks riched20
+wget https://github.com/tom-snow/wechat-windows-versions/releases/download/v3.0.0.57/WeChatSetup-3.0.0.57.exe
+wine WeChatSetup-3.0.0.57.exe
